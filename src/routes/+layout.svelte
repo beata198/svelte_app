271b823893bae25e013ca07dcb270cbc84e52233
beata198 <b.lublewska@gmail.com>
2024-11-05 18:07:39 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import logo from '$lib/assets/logo.png';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="flex justify-between px-8 py-4 text-xl">
+	<a href="/" class="flex items-center justify-center gap-3 text-lg font-semibold uppercase">
+		<img class="h-[50px]" src={logo} alt="Logo VoteApp - voting by placing a ballot into a box" />
+		<h1>VoteApp</h1>
+	</a>
+	<nav class="flex gap-6">
+		<a class="font-semibold hover:text-red-800" href="/">Home</a>
+		<a class="font-semibold hover:text-red-800" href="/About">About</a>
+	</nav>
+</header>
+<main class="m-auto mt-10 p-4 md:mt-[100px] md:w-3/4 md:p-0">
+	{@render children()}
+</main>
